1882eb31360bf30da6c30c3b203e99902edad3eb
Haraldur Ingi Shoshan <haraldurshoshan@gmail.com>
2024-01-24 21:41:15 +0000

    fix a logo, angantyr bætt við

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,6 +5,7 @@ import DrapaLogo from '@/public/drapa_logo.svg';
 import DrapaLogoNewBrons from '@/public/Drapa_logo_new_brons.png';
 import DrapaLogoNewWhite from '@/public/DrapaCLEAN_WHITE.svg';
 import DrapaLogoNewBronsSkraut from '@/public/Drapa_logo_brons_skraut.svg';
+import Angantyr from '@/public/angantyr_noLogoMix.png';
 import Image from 'next/image';
 import { useSectionInView } from '@/lib/hooks';
 
@@ -15,15 +16,16 @@ export default function Logo() {
 		<section
 			ref={ref}
 			id="home"
-			className="scroll-mt-[20rem] mb-[5rem] text-white"
+			className="scroll-mt-[20rem] mb-[5rem] text-white "
 		>
 			{/* <DrapaLogo /> */}
-			<Image
+			{/* <Image
 				priority
-				src={DrapaLogoNewBronsSkraut}
+				src={DrapaLogoNewWhite}
 				alt="Drapa Logo"
-				className="h-auto w-[20rem] sm:w-[80rem] md:w-[80rem] text-white"
-			/>
+				className="h-auto w-[20rem] sm:w-[80rem] md:w-[80rem] text-white sm:mt-52 md:mt-[36rem] opacity-60 z-50"
+			/> */}
+			<Image src={Angantyr} alt="Angantyr" className="h-auto sm:w-[80rem]" />
 		</section>
 	);
 }
